68fd8cae99b6a7645676619eabcdd1cd2dc7b1aa
cryptofluencerr <89958405+cryptofluencerr@users.noreply.github.com>
2021-10-19 18:54:49 +0530

    commit

---
+++ newsletter-signup/app.js
@@ -0,0 +1,11 @@
+const express = require("express");
+
+app = express();
+
+app.get("/", (req, res) => {
+  res.send("It is working");
+});
+
+app.listen(3000, () => {
+  console.log("Port is running on 3000");
+});
