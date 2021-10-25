9bcbe526be820944cd6719158e9c4d4167789d82
cryptofluencerr <89958405+cryptofluencerr@users.noreply.github.com>
2021-10-25 15:41:49 +0530

    update

--- fruits-project/app.js
+++ fruits-project/app.js
@@ -1,24 +1,30 @@
-const { MongoClient } = require('mongodb');
+const { MongoClient } = require("mongodb");
 // or as an es module:
 // import { MongoClient } from 'mongodb'
 
 // Connection URL
-const url = 'mongodb://localhost:27017';
+const url = "mongodb://localhost:27017";
 const client = new MongoClient(url);
-
-// Database Name
-const dbName = 'myProject';
 
 async function main() {
   // Use connect method to connect to the server
   await client.connect();
-  console.log('Connected successfully to server');
-  const db = client.db(dbName);
-  const collection = db.collection('documents');
+  console.log("Connected successfully to server");
+  const db = client.db("fruitsDB");
+  const collection = db.collection("fruits");
 
+  //   const insertResult = await collection.insertMany([
+  //     { name: "Apple", score: 8, review: "Great fruit" },
+  //     { name: "Orange", score: 6, review: "Kinda sour" },
+  //     { name: "Banana", score: 89, review: "Great " },
+  //   ]);
+  //   console.log("Inserted documents =>", insertResult);
+
+  const findResult = await collection.find({}).toArray();
+  console.log("Found documents =>", findResult);
   // the following code examples can be pasted here...
 
-  return 'done.';
+  return "done.";
 }
 
 main()
