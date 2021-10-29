d1f95f0d0d68d5c917e1333b681e6ed56bd76c13
cryptofluencerr <89958405+cryptofluencerr@users.noreply.github.com>
2021-10-29 15:47:04 +0530

    update

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -6,6 +6,18 @@ app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
+const homeStartingContent = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta tenetur quibusdam fuga minus iure incidunt, ipsum hic, vel obcaecati sequi dolorum necessitatibus, est voluptatum repellat alias quis velit itaque? Repellat.
+  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta tenetur quibusdam fuga minus iure incidunt, ipsum hic, vel obcaecati sequi dolorum necessitatibus, est voluptatum repellat alias quis velit itaque? Repellat.`;
+const aboutContent = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta tenetur quibusdam fuga minus iure incidunt, ipsum hic, vel obcaecati sequi dolorum necessitatibus, est voluptatum repellat alias quis velit itaque? Repellat.
+  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta tenetur quibusdam fuga minus iure incidunt, ipsum hic, vel obcaecati sequi dolorum necessitatibus, est voluptatum repellat alias quis velit itaque? Repellat.
+  Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta tenetur quibusdam fuga minus iure incidunt, ipsum hic, vel obcaecati sequi dolorum necessitatibus, est voluptatum repellat alias quis velit itaque? Repellat.`;
+const contactContent = `Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta tenetur quibusdam fuga minus iure incidunt, ipsum hic, vel obcaecati sequi dolorum necessitatibus, est voluptatum repellat alias quis velit itaque? Repellat.
+Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta tenetur quibusdam fuga minus iure incidunt, ipsum hic, vel obcaecati sequi dolorum necessitatibus, est voluptatum repellat alias quis velit itaque? Repellat.`;
+
+app.get("/", (req, res) => {
+  res.render("home", { homeStartingContent: homeStartingContent });
+});
+
 app.listen(3000, () => {
   console.log("Server started on 3000");
 });
