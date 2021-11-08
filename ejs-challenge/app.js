fa56aee984c99a0f1b8603d2f9058847b8d13233
cryptofluencerr <89958405+cryptofluencerr@users.noreply.github.com>
2021-11-08 16:13:44 +0530

    Update (EJS Challenge completed)

--- ejs-challenge/app.js
+++ ejs-challenge/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ejs = require("ejs");
+const _ = require("lodash");
 const app = express();
 
 app.set("view engine", "ejs");
@@ -15,7 +16,41 @@ const contactContent = `Lorem ipsum, dolor sit amet consectetur adipisicing elit
 Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dicta tenetur quibusdam fuga minus iure incidunt, ipsum hic, vel obcaecati sequi dolorum necessitatibus, est voluptatum repellat alias quis velit itaque? Repellat.`;
 
 app.get("/", (req, res) => {
-  res.render("home", { homeStartingContent: homeStartingContent });
+  res.render("home", {
+    homeStartingContent: homeStartingContent,
+    posts: posts,
+  });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about", { aboutContent: aboutContent });
+});
+
+app.get("/contact", (req, res) => {
+  res.render("contact", { contactContent: contactContent });
+});
+
+app.get("/compose", (req, res) => {
+  res.render("compose");
+});
+
+var posts = [];
+
+app.post("/compose", (req, res) => {
+  const post = { title: req.body.title, content: req.body.post };
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:postName", function (req, res) {
+  // console.log(req.params.postName);
+  // console.log(posts[0].title);
+  for (let i = 0; i < posts.length; i++) {
+    if (_.startCase(req.params.postName) === posts[i].title) {
+      const posting = posts[i];
+      res.render("post", { posting: posting });
+    }
+  }
 });
 
 app.listen(3000, () => {
