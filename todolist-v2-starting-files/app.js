76368ffda8298dcd070bb773244c4c7ad483043a
cryptofluencerr <89958405+cryptofluencerr@users.noreply.github.com>
2021-11-08 18:39:01 +0530

    update

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -1,31 +1,40 @@
-//jshint esversion:6
-
 const express = require("express");
-const bodyParser = require("body-parser");
-const date = require(__dirname + "/date.js");
+//require mongoose
+const mongoose = require("mongoose");
 
 const app = express();
 
-app.set('view engine', 'ejs');
+app.set("view engine", "ejs");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
+
+//selecting network for mongoDB and DB name
+mongoose.connect("mongodb://localhost:27017/todolistDB", {
+  useNewUrlParser: true,
+});
+//Schema for mongooseDB
+const itemSchema = new mongoose.Schema({
+  name: String,
+});
+//mongoose model
+const Item = mongoose.model("Item", itemSchema);
+const item1 = new Item({ name: "Welcome to your todoList!" });
+const item2 = new Item({ name: "Hit the + button to add a new item." });
+const item3 = new Item({ name: "<-- Hit this to delete an item." });
+
+const defaultItems = [item1, item2, item3];
+
 
 const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
 
-app.get("/", function(req, res) {
-
-const day = date.getDate();
-
-  res.render("list", {listTitle: day, newListItems: items});
-
+app.get("/", function (req, res) {
+  res.render("list", { newListItems: items });
 });
 
-app.post("/", function(req, res){
-
+app.post("/", function (req, res) {
   const item = req.body.newItem;
-
   if (req.body.list === "Work") {
     workItems.push(item);
     res.redirect("/work");
@@ -35,14 +44,14 @@ app.post("/", function(req, res){
   }
 });
 
-app.get("/work", function(req,res){
-  res.render("list", {listTitle: "Work List", newListItems: workItems});
+app.get("/work", function (req, res) {
+  res.render("list", { listTitle: "Work List", newListItems: workItems });
 });
 
-app.get("/about", function(req, res){
+app.get("/about", function (req, res) {
   res.render("about");
 });
 
-app.listen(3000, function() {
+app.listen(3000, function () {
   console.log("Server started on port 3000");
 });
