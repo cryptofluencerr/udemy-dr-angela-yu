cbe727fe6721ef16c6b4c7f33aa9fffdc09787d3
cryptofluencerr <89958405+cryptofluencerr@users.noreply.github.com>
2021-11-09 18:11:42 +0530

    update

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -1,14 +1,11 @@
 const express = require("express");
 //require mongoose
 const mongoose = require("mongoose");
-
 const app = express();
 
 app.set("view engine", "ejs");
-
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
-
 //selecting network for mongoDB and DB name
 mongoose.connect("mongodb://localhost:27017/todolistDB", {
   useNewUrlParser: true,
@@ -25,23 +22,39 @@ const item3 = new Item({ name: "<-- Hit this to delete an item." });
 
 const defaultItems = [item1, item2, item3];
 
-
-const items = ["Buy Food", "Cook Food", "Eat Food"];
+// const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
 
 app.get("/", function (req, res) {
-  res.render("list", { newListItems: items });
-});
+  // Mongoose method to add items
+  Item.find((err, docs) => {
+    if (docs.length === 0) {
+      Item.insertMany(defaultItems, (err) => {
+        if (err) {
+          console.log(err);
+        } else {
+          console.log("Updated successfully");
+        }
+      });
+    } else {
+      console.log(defaultItems);
+      res.render("list", {
+        newListItems: defaultItems,
+        listTitle: "Todo List",
+      });
+    }
+  });
 
-app.post("/", function (req, res) {
-  const item = req.body.newItem;
-  if (req.body.list === "Work") {
-    workItems.push(item);
-    res.redirect("/work");
-  } else {
-    items.push(item);
-    res.redirect("/");
-  }
+  app.post("/", function (req, res) {
+    const item = req.body.newItem;
+    if (req.body.list === "Work") {
+      workItems.push(item);
+      res.redirect("/work");
+    } else {
+      items.push(item);
+      res.redirect("/");
+    }
+  });
 });
 
 app.get("/work", function (req, res) {
