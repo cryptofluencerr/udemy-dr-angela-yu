4721e7cc05ed92f1132bcddff31fbcd5c0033523
cryptofluencerr <89958405+cryptofluencerr@users.noreply.github.com>
2021-11-10 17:27:43 +0530

    update

--- todolist-v2-starting-files/app.js
+++ todolist-v2-starting-files/app.js
@@ -19,8 +19,10 @@ const Item = mongoose.model("Item", itemSchema);
 const item1 = new Item({ name: "Welcome to your todoList!" });
 const item2 = new Item({ name: "Hit the + button to add a new item." });
 const item3 = new Item({ name: "<-- Hit this to delete an item." });
-
 const defaultItems = [item1, item2, item3];
+
+const listSchema = new mongoose.Schema({ name: String, items: [itemSchema] });
+const List = mongoose.model("List", listSchema);
 
 // const items = ["Buy Food", "Cook Food", "Eat Food"];
 const workItems = [];
@@ -48,19 +50,52 @@ app.get("/", function (req, res) {
 
 app.post("/", function (req, res) {
   const itemName = req.body.newItem;
-  if (itemName === "") {
-    window.alert("Donot leave the space blank");
-  } else {
-    const item = new Item({
-      name: itemName,
-    });
+  const listName = req.body.list;
+  const item = new Item({
+    name: itemName,
+  });
+  if (listName === "Today") {
     item.save();
     res.redirect("/");
+  } else {
+    List.findOne({ name: listName }, function (err, foundList) {
+      foundList.items.push(item);
+      foundList.save();
+      console.log(foundList, listName);
+      res.redirect("/" + listName);
+    });
   }
 });
 
-app.get("/work", function (req, res) {
-  res.render("list", { listTitle: "Work List", newListItems: workItems });
+app.post("/delete", (req, res) => {
+  const checkedItemId = req.body.checkbox;
+  Item.findByIdAndRemove(checkedItemId, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Deleted successfully");
+    }
+    res.redirect("/");
+  });
+});
+
+app.get("/:customListName", (req, res) => {
+  // console.log(req.params.customListName);
+  const customListName = req.params.customListName;
+  List.findOne({ name: customListName }, function (err, foundList) {
+    if (!err) {
+      if (!foundList) {
+        const list = new List({ name: customListName, items: defaultItems });
+        list.save();
+        res.redirect("/" + customListName);
+      } else {
+        res.render("list", {
+          listTitle: foundList.name,
+          newListItems: foundList.items,
+        });
+      }
+    }
+  });
 });
 
 app.get("/about", function (req, res) {
