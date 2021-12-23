f8e2e21e531a4be29fb46a80119cf7f177634689
cryptofluencerr <89958405+cryptofluencerr@users.noreply.github.com>
2021-12-23 18:44:23 +0530

    update

--- wiki-api/app.js
+++ wiki-api/app.js
@@ -1,18 +1,57 @@
 const express = require("express");
 const https = require("https");
+const ejs = require("ejs");
+const mongoose = require("mongoose");
 
 app = express();
-
+app.set("view engine", "ejs");
 app.use(express.urlencoded({ extended: true }));
-
 // To render static files
 app.use(express.static("public"));
 
-app.get("/", (req, res) => {
-  res.sendFile(__dirname + "/signup.html");
+mongoose.connect("mongodb://localhost:27017/wikiDB", {
+  useNewUrlParser: true,
 });
 
-app.post("/", (req, res) => {});
+const articleSchema = {
+  title: String,
+  content: String,
+};
+const Article = mongoose.model("Article", articleSchema);
+
+app.get("/articles", function (req, res) {
+  Article.find(function (err, foundArticles) {
+    if (!err) {
+      res.send(foundArticles);
+    } else {
+      res.send(err);
+    }
+  });
+});
+
+app.post("/articles", (req, res) => {
+  const newArticle = new Article({
+    title: req.body.title,
+    content: req.body.content,
+  });
+  newArticle.save(function (err) {
+    if (!err) {
+      res.send("Successfully added a new article");
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.delete("/articles", (req, res) => {
+  Article.deleteMany(function (err) {
+    if (!err) {
+      res.send("Successfully selected the articles.");
+    } else {
+      res.send(err);
+    }
+  });
+});
 
 app.listen(3000, () => {
   console.log("Port is running on 3000");
