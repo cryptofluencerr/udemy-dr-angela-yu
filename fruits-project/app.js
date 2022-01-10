df20128a1df21a9b31ab042bd00ec5137f6e9cfe
kcpython <kcbizzkc@gmail.com>
2022-01-10 23:39:37 +0530

    update

--- fruits-project/app.js
+++ fruits-project/app.js
@@ -7,7 +7,7 @@ mongoose.connect("mongodb://localhost:27017/fruitsDB", {
 const fruitSchema = new mongoose.Schema({
   name: {
     type: String,
-    required: true,
+    required: [true, "Please check your data entry no name specified"],
     unique: true,
   },
   rating: {
@@ -19,23 +19,37 @@ const fruitSchema = new mongoose.Schema({
 });
 
 const Fruit = mongoose.model("Fruit", fruitSchema);
-const fruit = new Fruit({
-  name: "Apple",
-  rating: 10,
-  review: "Pretty solid as a fruit.",
-});
+// const fruit = new Fruit({
+//   name: "asdad",
+//   rating: 10,
+//   review: "asdsa asdsad as a sdasada.",
+// });
 
-// fruit.save();
+// Fruit.insertMany(
+//   [{ name: "dfsfsdf", rating: 5, review: "fdsdsf sdfsdfsd!" }],
+//   (err) => {
+//     if (err) {
+//       console.log(err);
+//     } else {
+//       console.log("Successfully saved all the fruits to fruitsDB");
+//     }
+//   }
+// );
+
+// Fruit.deleteOne({ name: "dfsfsdf" }, (err) => {
+//   if (err) {
+//     console.log(err);
+//   } else {
+//     console.log("deleted successfully!!");
+//   }
+// });
 
 Fruit.find(function (err, fruits) {
   if (err) {
     console.log(err);
   } else {
-    // for (let i = 0; i < fruits.length; i++) {
-    //   console.log(fruits[i].name);
-    // }
     fruits.forEach(function (fruit) {
-      console.log(fruit);
+      console.log(fruit.name);
     });
     mongoose.connection.close();
     console.log(fruits.length);
